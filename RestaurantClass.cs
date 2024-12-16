3420ed0e98ade5399e3513c0094404da60bc186c
Arthur41k <23fmif.a.paskhalin@std.udu.edu.ua>
2024-12-16 20:10:39 +0200

    Виніс кожен клас в окремий solution, а метод мейн в internal class Program

---
+++ RestaurantClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRestaurantTableReservationManager
+{
+    public class RestaurantClass
+    {
+        public string n; //name
+        public RestaurantTableClass[] t; // tables
+    }
+}
---
+++ RestaurantTableClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRestaurantTableReservationManager
+{
+    public class RestaurantTableClass
+    {
+        private List<DateTime> bd; //booked dates
+
+
+        public RestaurantTableClass()
+        {
+            bd = new List<DateTime>();
+        }
+
+        // book
+        public bool Book(DateTime d)
+        {
+            try
+            {
+                if (bd.Contains(d))
+                {
+                    return false;
+                }
+                //add to bd
+                bd.Add(d);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error");
+                return false;
+            }
+        }
+
+        // is booked
+        public bool IsBooked(DateTime d)
+        {
+            return bd.Contains(d);
+        }
+    }
+}
